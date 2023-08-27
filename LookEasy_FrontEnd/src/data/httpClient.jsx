33f26a8683fcb037ae7f68ecd38f5ed4d263af8d
Juan E. GIl <96081036+JuanE-Gil@users.noreply.github.com>
2023-08-27 15:32:08 -0500

    Update httpClient.jsx

--- LookEasy_FrontEnd/src/data/httpClient.jsx
+++ LookEasy_FrontEnd/src/data/httpClient.jsx
@@ -4,7 +4,7 @@ export function get(path) {
     return fetch(API + path + Languages_es, {
         headers: {
             Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZTIzOWU2NTlhZTRjMDU3OWI0NTkzYzAzNTY0MjEyMyIsInN1YiI6IjY0OWRjMTU2YzA3MmEyMDBlY2U0NTMwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W6pWmeuMPpYWSp2CRLAS9D-pHRkmAFuQh7k72TBDqpk',
+                'Bearer ',
             'Content-Type': 'application/json;charset=utf-8',
         },
     }).then((result) => result.json());
